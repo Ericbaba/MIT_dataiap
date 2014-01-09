282026f600bb215125a6d2a4173a7cbebcb0f336
Bing Liu <v.bingliu@gmail.com>
2014-01-09 14:45:33 -0800

    day2 ex2

--- day2/plt_cumulative_donation_line.py
+++ day2/plt_cumulative_donation_line.py
@@ -15,45 +15,41 @@ os.chdir('/home/bing/Projects/dataiap')
 #reader = csv.DictReader(open(sys.argv[1], 'r'))
 #reader = csv.DictReader(open('datasets/donations/donations_sampled.csv', 'r'))
 reader = csv.DictReader(open('datasets/donations/donations.csv', 'r'))
-obamadonations = defaultdict(lambda:0)
-mccaindonations = defaultdict(lambda:0)
+obamadonations = list()
+mccaindonations = list()
 
 for row in reader:
     name = row['cand_nm']
-    datestr = row['contb_receipt_dt']
     amount = float(row['contb_receipt_amt'])
-    date = datetime.datetime.strptime(datestr, '%d-%b-%y')
     
     if 'Obama' in name:
-        obamadonations[date] += amount
+        obamadonations.append(amount)
     
     if 'McCain' in name:
-        mccaindonations[date] += amount
+        mccaindonations.append(amount)
     
-## dictionaries
-def sorted_by_amount(donations):
-    return (sorted(donations, key=lambda (key, value):value))
 
-sorted_by_amount_obama = sorted_by_amount(obamadonations.items())
-date1, sorted_donation_obama = zip(*sorted_by_amount_obama)
-sorted_by_amount_mccain = sorted_by_amount(mccaindonations.items())
-date2, sorted_donation_mccain = zip(*sorted_by_amount_mccain)
-#filtered_donation_obama = filter(lambda (x,y): y>=-18000 and y<=19000, obamadonations.items())
-#filtered_donation_mccain = filter(lambda (x,y): y>=-22000 and y<=22000, mccaindonations.items())
+obamadonations = sorted(obamadonations)
+mccaindonations = sorted(mccaindonations)
+#obamadonations = filter(lambda x: x>= -18000 and x<=19000, obamadonations)
+#mccaindonations = filter(lambda x: x>= -22000 and x<=22000, mccaindonations)
 
-bucket_size = 1000
-range_obama = range(int(min(sorted_donation_obama)/bucket_size), int(max(sorted_donation_obama)/bucket_size))
-range_mccain = range(int(min(sorted_donation_mccain)/bucket_size), int(max(sorted_donation_mccain)/bucket_size))
+bucket_size = 100
+range_obama = range(int(min(obamadonations)/bucket_size), int(max(obamadonations)/bucket_size))
+range_mccain = range(int(min(mccaindonations)/bucket_size), int(max(mccaindonations)/bucket_size))
 
 def cumulated_donation_uptoamt(donations, donation_range):
     cumulated_donation = list()
+    print('Total index number: %d' % len(donation_range)) 
+    print('Now processing...')        
     for index in range(0, len(donation_range)):
-        cumulated_donation_amt = sum(filter(lambda x: x<=donation_range[index]*1000, donations))
+        print('Index %d' % index)
+        cumulated_donation_amt = sum(filter(lambda x: x<=donation_range[index]*bucket_size, donations))
         cumulated_donation.append(cumulated_donation_amt)
     return (cumulated_donation)
 
-cumulated_donation_uptoamt_obama = cumulated_donation_uptoamt(sorted_donation_obama, range_obama)
-cumulated_donation_uptoamt_mccain = cumulated_donation_uptoamt(sorted_donation_mccain, range_mccain)
+cumulated_donation_uptoamt_obama = cumulated_donation_uptoamt(obamadonations, range_obama)
+cumulated_donation_uptoamt_mccain = cumulated_donation_uptoamt(mccaindonations, range_mccain)
 
 
 xs1,ys1 = [amt*bucket_size for amt in range_obama], cumulated_donation_uptoamt_obama
@@ -68,7 +64,8 @@ width = 0.25
 subplot = fig.add_subplot(111)
 subplot.plot(xs1, ys1,  color='blue', label='Obama\'s Donations')
 subplot.plot([x+width for x in xs2], ys2, color='green', label='McCain\'s Donations')
-subplot.legend(loc='upper left', ncol=1)
+subplot.legend(loc='upper right', ncol=1)
 subplot.set_title('Donation Cumulated-to-amount')
 subplot.set_xlabel('Donation Amount')
-plt.savefig('day2/donation_cumulated.png', format='png')
+#plt.savefig('day2/donation_cumulated.png', format='png')
+plt.savefig('day2/donation_cumulated_filtered.png', format='png')
